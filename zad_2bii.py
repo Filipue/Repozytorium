641de7294a1672c12ed065a1fc456082ec66ac4d
Filipue <filip.borysik@edu.uekat.pl>
2021-11-07 18:28:20 +0100

    usuniecie zbednych plikow

--- zad_2bii.py
+++
@@ -1,7 +0,0 @@
-#Lab2_zad2bii
-def zad_2bii(liczby):
-    pomnozone = [x*2 for x in liczby]
-    return pomnozone
-if __name__=='__main__':
-    liczby = [1, 2, 3, 4, 5]
-    print(zad_2bii(liczby))
--- zad_2c.py
+++
@@ -1,8 +0,0 @@
-#Lab2_zad2c
-def Zad_2c(liczby):
-    for x in range(len(liczby)):
-        if liczby[x] % 2 == 0:
-            print(f"liczby parzyste {liczby[x]}")
-if __name__=='__main__':
-    liczby = [1,2,3,4,5,6,7,8,9,10]
-    Zad_2c(liczby)
