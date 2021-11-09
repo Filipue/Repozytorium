e123f62c588482bc32bb4d2d98900a060378cc67
Filipue <filip.borysik@edu.uekat.pl>
2021-11-09 14:42:32 +0100

    Kolokwium 1

--- Zad.py
+++ Zad.py
@@ -3,6 +3,6 @@ def zad_6(lista1: list, lista2: list) -> list:
 
 if __name__ == '__main__':
     lista = [2, 4, 6, 8, 10]
-    x = [6,2,3,4,5,6,7,11]
+    lista2 = [6,2,3,4,5,6,7,11]
     zad_6(lista1, lista2)
     print(zad_6(lista1,lista2))
