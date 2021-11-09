561aff016f69f1c6612e443c2c562fcf9b6a04e7
Filipue <filip.borysik@edu.uekat.pl>
2021-11-09 10:36:45 +0100

    usuniecie

--- zad5.py
+++
@@ -1,8 +0,0 @@
-#Lab3_zad6
-
-def zad_6(arg1: list, arg2: list) -> list:
-    return list(x ** 3 for x in set(arg1+arg2))
-if __name__ == '__main__':
-    arg1 = [3, 5, 7, 9, 10]
-    arg2 = [1,23,4,5,11]
-    print(zad_6(arg1,arg2))
