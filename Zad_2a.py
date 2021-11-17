ada132dc436e6e5ab722ed8b9097eba8aff794f3
Filipue <57937089+Filipue@users.noreply.github.com>
2021-11-17 18:44:18 +0100

    Add files via upload

---
+++ Zad_2a.py
@@ -0,0 +1,7 @@
+#Lab2_zad2a
+def zad_2a(imiona):
+    for x in imiona:
+        print(x)
+if __name__ == '__main__':
+    imiona = ["Damian", "Kuba", "Krzysiek", "Adam", "Marek"]
+    zad_2a(imiona)
