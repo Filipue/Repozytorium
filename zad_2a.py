44fdfc6a1655f9a6f04b26ab624b8427c1a52134
Filipue <57937089+Filipue@users.noreply.github.com>
2021-11-17 18:43:53 +0100

    Delete zad_2a.py

--- zad_2a.py
+++
@@ -1,9 +0,0 @@
-#Lab2_zad2bi
-def zad_2bi(liczby):
-    pomnozone = []
-    for x in liczby:
-        pomnozone.append(x*2)
-    return pomnozone
-if __name__=='__main__':
-    liczby = [1, 2, 3, 4, 5]
-    print(zad_2bi(liczby))
