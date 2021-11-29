af7fd86b9045a52f11cc93fb7d84120eaa1f0181
Filipue <57937089+Filipue@users.noreply.github.com>
2021-11-29 16:24:23 +0100

    Delete Product.py

--- Product.py
+++
@@ -1,8 +0,0 @@
-from magazine import utilis
-
-class Product:
-    def __init__(self, id_num: str, name: str):
-        self.id_num = id_num
-        self.name = name
-        utilis.test("test")
-        
