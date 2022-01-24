be006461bb3feafbb423133d2a16020daf9a8e0f
Filipue <filip.borysik@edu.uekat.pl>
2022-01-24 22:47:59 +0100

    person detection

---
+++ main.py
@@ -0,0 +1,38 @@
+import os as o
+import cv2 as cv
+import imutils as im
+
+
+hog = cv.HOGDescriptor()
+hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
+
+
+def read_data():
+    data_format = [".jpg"]
+    path = 'Photos/'
+    for (path, dirs, data) in o.walk(path):
+        for photo in data:
+            if photo.endswith(tuple(data_format)):
+                print(photo)
+                photo = path + photo
+                detection(photo)
+
+
+def detection(photo: str):
+    photo = cv.imread(photo)
+    photo = im.resize(photo, width=min(900, photo.shape[1]))
+    (regions, _) = hog.detectMultiScale(photo, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+    register = 1
+    for (x, y, w, h) in regions:
+        cv.rectangle(photo, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv.putText(photo, f'person {register}', (x, y), cv.FONT_HERSHEY_TRIPLEX, 0.5, (0, 0, 255), 2)
+        register += 1
+
+    print(f'On the image are {register-1} human`s')
+    cv.imshow("Photo", photo)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+read_data()
